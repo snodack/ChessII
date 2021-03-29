02c51d025099e9f714df062bcb58d037c73f7161
Evgeny <computer.zhen@gmail.com>
2021-03-29 16:39:02 +0300

    add castling for white

--- core_chess.py
+++ core_chess.py
@@ -6,23 +6,26 @@ Rook = 2 # вроде работает
 Bishop = 3 # вроде работает
 Knight = 4 # вроде работает
 tm = time.time()
-def find_chess_moves(player_color, position):
+def find_chess_moves(player_color, position, castling_data = (True, True)):
     all_exists_moves = []
     color_char = 'w' if player_color else 'b'
     for file in range(8):
         for rank in range(8):
             pos_fig = position[file][rank]
             if pos_fig != None and pos_fig[0] == color_char:
-                if pos_fig[1] == 'P':
+                figure_char = pos_fig[1]
+                if figure_char == 'P':
                     all_exists_moves += find_pawn_moves(player_color, position, rank, file)
-                elif pos_fig[1] == 'N':
+                elif figure_char == 'N':
                     all_exists_moves += find_knight_moves(player_color, position, rank, file)
-                elif pos_fig[1] == 'R':
+                elif figure_char == 'R':
                     all_exists_moves += find_rook_moves(player_color, position, rank, file)
-                elif pos_fig[1] == 'B':
+                elif figure_char == 'B':
                    all_exists_moves += find_bishop_moves(player_color, position, rank, file)
-                if pos_fig[1] == 'Q': 
+                if figure_char == 'Q': 
                     all_exists_moves += find_queen_moves(player_color, position, rank, file)
+                if figure_char == 'K':
+                    all_exists_moves += find_king_moves(player_color, position, rank, file, castling_data)
     return all_exists_moves
 
 #ran3k - A-H, fi3les = 1-8
@@ -145,6 +148,8 @@ def find_rook_moves(player_color, position, rank, file):
             elif cell_have_chessman(oponent_color, position[file][rank + i], False):
                 all_rock_moves.append(current_pos_dig_not*100 + digital_notation(file,rank + i))
                 break
+            else:
+                break
         else:
             break
     for i in ([j for j in range(1, 8)]):
@@ -153,6 +158,8 @@ def find_rook_moves(player_color, position, rank, file):
                 all_rock_moves.append(current_pos_dig_not*100 + digital_notation(file,rank + i))
             elif cell_have_chessman(oponent_color, position[file][rank + i], False):
                 all_rock_moves.append(current_pos_dig_not*100 + digital_notation(file,rank + i))
+                break
+            else:
                 break
         else:
             break
@@ -198,9 +205,34 @@ def find_pawn_moves(player_color, position, rank, file):
             all_pawn_moves.append(current_pos_dig_not*100 + digital_notation(file + player_move_direction,rank - 1))
     return all_pawn_moves
 
-def find_king_moves(player_color, position, rank, file):
+def find_king_moves(player_color, position, rank, file, castling_data):
+    oponent_color = 'b' if player_color else 'w'
+    current_pos_dig_not = digital_notation(file,rank)
     all_king_moves = []
-    kings_default_mos = [[1,0], [1,0],]
+    kings_default_move = [[1,-1], [1,0], [1,1],
+                         [0,-1], [0,1],
+                         [-1,-1], [-1,0], [-1,1]]
+    #Обычные  ходы
+    for moves in kings_default_move:
+        if (0<= file + moves[0] <= 7 and
+            0<= rank + moves[1] <= 7):
+            if cell_have_chessman(oponent_color, position[file + moves[0]][rank + moves[1]]):
+                all_king_moves.append(current_pos_dig_not*100 + digital_notation(file + moves[0], rank + moves[1]))
+    #Рокировка
+    #Короткая 0-0
+    if (castling_data[0] and
+        position[file][rank + 1] == None and
+        position[file][rank + 2] == None):
+        all_king_moves.append("00")
+    #Длинная 0-0-0
+    if (castling_data[1] and
+        position[file][rank - 1] == None and
+        position[file][rank - 2] == None and
+        position[file][rank - 3 ] == None):
+        all_king_moves.append("000")
+
+
+    return all_king_moves
 
 
     
@@ -221,10 +253,10 @@ def cell_have_chessman(color_char, cell, allow_none = True):
 
 
 
-start_position = [["wR","wN","wB","wQ","wK","wB","wN","wR"],
+start_position = [["wR",None,None,None,"wK",None,"wN","wR"],
                     ["wP","wP","wP","wP","wP","wP","wP","wP"],
                     [None,None,None,None,None,None,None,None],
-                    [None,None,None,None,"wN",None,None,None],
+                    [None,None,None,None,None,None,None,None],
                     [None,None,None,None,None,None,None,None],
                     [None,None,None,None,None,None,None,None],
                     ["bP","bP","bP","bP","bP","bP","bP","bP"],
